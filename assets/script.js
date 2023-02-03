0ae4349dd9be5e6be06feadf39cc081efe2394a5
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-03 18:24:46 +0000

    added starter files

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+
+var APIkey = "4b2e3f9a3ae657e2dfba8a0d34ce775e" ;
+
+var cityname = "london";
+
+var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityname + "&limit=5&appid=" + APIkey;
