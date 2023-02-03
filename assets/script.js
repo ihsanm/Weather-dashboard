2a9ed9269f56ed78e42619cfbfc17b1d68c10414
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-03 19:14:59 +0000

    added 2 ajax calls, one that gets the latitude and longitude and the other with gets information on the forecast

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
+// variables for apikeys, city names and url
 
 var APIkey = "4b2e3f9a3ae657e2dfba8a0d34ce775e" ;
 
 var cityname = "london";
 
 var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityname + "&limit=5&appid=" + APIkey;
+
+
+
+
+// ajax response
+$.ajax({
+    url: geocodeURL,
+    method:"GET"
+}).then(function(response){
+
+    var lat = response[0].lat;
+    var lon = response[0].lon;
+    console.log(lat)
+    console.log(lon)
+    
+
+    var forecast = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&appid=" + APIkey;
+    $.ajax({
+        url: forecast,
+        method:"GET"
+    }).then(function(result){
+        console.log(result)
+    });
+});
+
