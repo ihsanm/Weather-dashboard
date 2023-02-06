18f800e9d1bce59a9e2af9c8c628533c8a48acbb
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-06 17:44:16 +0000

    added code to update the today section with text containing city name temp wind speed and humidity and added cards to html and code to css to style cards and today section with border and padding

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,23 @@
+
+
+// Click event when search button is clicked to trigger ajax response
+
+$("#search-button").on("click", function(event){
+
+    event.preventDefault();
+
 // variables for apikeys, city names and url
 
 var APIkey = "4b2e3f9a3ae657e2dfba8a0d34ce775e" ;
 
-var cityname = "london";
+var cityname = $("#search-input").val().trim();
 
 var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityname + "&limit=5&appid=" + APIkey;
 
+$("#search-input").val("");
 
 
-
-// ajax response
+// 2 ajax responses one getting lat and longitude and the other to use the information to gather the forecast
 $.ajax({
     url: geocodeURL,
     method:"GET"
@@ -27,6 +35,19 @@ $.ajax({
         method:"GET"
     }).then(function(result){
         console.log(result)
+
+        var todaycityname = $("<h1>").text(result.city.name);
+        var temp = $("<p>").text(result.list[0].main.temp)
+        var wind = $("<p>").text(result.list[0].wind.speed)
+        var humidity = $("<p>").text(result.list[0].main.humidity)
+
+        $("#today").append(todaycityname, temp, wind, humidity);
+
+        $("#today").addClass("borderstyling")
     });
 });
+ 
+
+})
+
 
